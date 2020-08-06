fdbfab821c87dae7a1ca90c879c440c071c06ca6
Anton Agency <info@anton.agency>
2020-08-06 17:49:19 -0400

    Removed innecesary const

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import {Dimensions} from "react-native";
 
-export const SCREEN_WIDTH = Dimensions.get("screen").width;
+
 export const DEFAULT_LINE_COLOR = "#355587";
 export const DEFAULT_CHANGE_DURATION = 500;
